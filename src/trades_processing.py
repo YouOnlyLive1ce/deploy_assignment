8677996186f9da68bf7251d70d9158699cb3e2f6
YouOnlyLive1ce <dimanekrasov340@gmail.com>
2024-09-19 21:39:13 +0300

    poc model

--- src/trades_processing.py
+++ src/trades_processing.py
@@ -2,46 +2,70 @@ import os
 import glob
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 def process_trades_in_chunks(input_file, output_file_train, output_file_test, chunksize=100000):
     # Open the output files
     with open(output_file_train, 'w', newline='') as outfile_train, open(output_file_test, 'w', newline='') as outfile_test:
         for chunk in pd.read_csv(input_file, chunksize=chunksize, header=None):
-            # Assuming the following index mapping based on the CSV structure:
+            # Assuming the following index mapping based on Binance aggTrades CSV:
             # Column indices:
-            # 0 - trade_id (if exists)
+            # 0 - trade_id
             # 1 - price
             # 2 - qty
-            # 3 - quote_qty
-            # 4 - timestamp
-            # 5 - isBuyerMaker
-            # 6 - isBestMatch
-                        
-            if chunk.shape[1] > 0:  # Check if the chunk has any columns
-                chunk = chunk.drop(columns=[0])  # Drop the first column (trade_id)
+            # 3 - first_trade_id
+            # 4 - last_trade_id
+            # 5 - timestamp
+            # 6 - isBuyerMaker
+            # 7 - isBestMatch
 
-                chunk[2] = chunk[2].round(3)  # Round quantity
-                chunk[3] = chunk[3].round(0)  # Round quote_qty
+            # Round 'price' to the nearest 100 using .mod() for better precision
+            chunk[1] = chunk[1].round(-2)
 
-            # Round the price to the nearest 10 and convert it to an integer
-            chunk[1] = chunk[1].round(-1).astype(int)
+            # Calculate percent till nearest 1000
+            chunk['percent_to_1000'] = (chunk[1]/1000).round(-4)
 
-            # Create bins for quote_qty
-            bins = [0, 50, 250, 500, 1000, 3000, 200000]
-            labels = ['0-50', '50-250', '250-500', '500-1000', '1000-3000', '3000-200000']
-            chunk['quote_qty_bins'] = pd.cut(chunk[3], bins=bins, labels=labels, include_lowest=True)
-            chunk=chunk.drop(columns=[3,4,6])
+            # Add a column for the number of aggregated trades
+            chunk['aggregated_trades'] = chunk[4] - chunk[3] + 1
 
-            # Split into train/test (90/10 split)
-            train, test = train_test_split(chunk, test_size=0.1)
+            # Add a column to check if the price has appeared before (sequential)
+            chunk['price_seen_before'] = chunk[1].duplicated(keep='first')
 
+            # Drop unwanted columns: 0 (trade_id), 3 (first_trade_id), 4 (last_trade_id), 5 (timestamp), 6 (isBuyerMaker), 7 (isBestMatch)
+            chunk = chunk.drop(columns=[0, 3, 4, 5])
+
+            chunk[2]=chunk[2].round(3)
+
+            # Delete 90% of rows where the quantity (column 2) is <0.0005
+            zero_quantity_rows = chunk[chunk[2] < 0.001]
+            chunk = chunk.drop(zero_quantity_rows.sample(frac=0.99).index)
+
+            middle_quantity_rows = chunk[chunk[2] < 0.5]
+            chunk = chunk.drop(middle_quantity_rows.sample(frac=0.99).index)
+            
+            # Normalize the data
+            chunk.columns = chunk.columns.astype(str)
+            scaler = StandardScaler()
+            chunk[['1','2']] = scaler.fit_transform(chunk[['1','2']]).astype('float32')
+            chunk['1']=chunk['1'].round(3).astype('float32')
+            chunk['2']=chunk['2'].round(3).astype('float32')
+            chunk['6']=chunk['6'].astype('int32')
+            chunk['7']=chunk['7'].astype('int32')
+            chunk['percent_to_1000']=chunk['percent_to_1000'].astype('int32')
+            chunk['price_seen_before'] = chunk['price_seen_before'].astype('int32')
+
+            train, test = train_test_split(chunk, test_size=0.2)
+            
             # Write train and test chunks to respective output files
-            train.to_csv(outfile_train, header=outfile_train.tell() == 0, index=False)
-            test.to_csv(outfile_test, header=outfile_test.tell() == 0, index=False)
+            train_df = pd.DataFrame(train)  # Convert numpy array back to DataFrame
+            test_df = pd.DataFrame(test)
+
+            train_df.to_csv(outfile_train, header=outfile_train.tell() == 0, index=False)
+            test_df.to_csv(outfile_test, header=outfile_test.tell() == 0, index=False)
 
 def main():
     input_folder = 'data/raw/'
-    input_files = glob.glob(os.path.join(input_folder, '*trades*'))
+    input_files = glob.glob(os.path.join(input_folder, '*aggTrades*'))
 
     output_folder = 'data/processed'
     os.makedirs(output_folder, exist_ok=True)
@@ -58,3 +82,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# TODO: transformer scaler to fix curve ditribution
